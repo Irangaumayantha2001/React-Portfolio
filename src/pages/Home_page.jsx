ebfbaf2e5fd945018f0b814687841acaee42cdb0
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-18 21:22:48 +0530

    add contact and update assets

--- src/pages/Home_page.jsx
+++ src/pages/Home_page.jsx
@@ -5,15 +5,16 @@ import About from "../components/first_section_about/About";
 import AboutSec from "../components/about_section/About";
 import Projects from "../components/projects_section/Projects";
 import Skills from "../components/skills_section/Skills";
+import Contact from "../components/contact_section/Contact";
 
 const Home_page = () => {
   return (
     <>
       <About />
-      <AboutSec/>
-      <Skills/>
-      <Projects/>
-
+      <AboutSec />
+      <Skills />
+      <Projects />
+      <Contact />
     </>
   );
 };
