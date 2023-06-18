d53c7c7b9cd69005e9ae99a21e724d65617c2ed1
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-19 01:01:32 +0530

    add resume

--- src/components/first_section_about/About.jsx
+++ src/components/first_section_about/About.jsx
@@ -1,3 +1,4 @@
+/** @format */
 
 const About = () => {
   return (
@@ -11,24 +12,31 @@ const About = () => {
           <p className="text-[#ABB2BF] my-6">
             He crafts responsive websites where technologies meet creativity
           </p>
-          <button className="text-white font-medium py-2 px-4 border duration-200 border-[#7FFF00] hover:bg-[#7FFF00]">
-            Contact me!!
-          </button>
+          <a href="https://drive.google.com/file/d/1Nv9tddhfPsKF24ceQgUJBcTdCtapwC14/view?usp=sharing">
+            <button className="text-white font-medium py-2 px-4 border duration-200 border-[#7FFF00] hover:bg-[#7FFF00]">
+              Hire me!!
+            </button>
+          </a>
         </div>
-        <div  data-aos="fade-left" data-aos-delay="400" className="mx-auto">
+        <div data-aos="fade-left" data-aos-delay="400" className="mx-auto">
           <div className="">
             <img src={require("../../assets/man1.png")} alt="man1" />
           </div>
           <div className="border flex items-center gap-2 border-[#ABB2BF] p-2 text-[#ABB2BF]">
             <div className="w-4 h-4 bg-[#7FFF00]"></div>
             <div className="">
-              Currently Learn on <span className="text-white">Frontend Framework</span>
+              Currently Learn on{" "}
+              <span className="text-white">Frontend Framework</span>
             </div>
           </div>
         </div>
       </div>
-      <div  data-aos="fade-up" className=" px-5 py-10">
-        <img className="mx-auto" src={require("../../assets/quote.png")} alt="quote" />
+      <div data-aos="fade-up" className=" px-5 py-10">
+        <img
+          className="mx-auto"
+          src={require("../../assets/quote.png")}
+          alt="quote"
+        />
       </div>
     </>
   );
